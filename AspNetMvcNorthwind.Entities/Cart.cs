c7c209199222f583f013db2c06801251beb6b904
gokerhabib <gokerhabib@gmail.com>
2020-07-22 22:19:26 +0300

    Sepete Ürün Ekleme ve Çıkarma Sesion ile Birlikte

---
+++ AspNetMvcNorthwind.Entities/Cart.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspNetMvcNorthwind.Entities
+{
+    public class Cart
+    {
+        List<CartLine> _lines = new List<CartLine>();
+
+        public void AddToCart(Product product, int quantity)
+        {
+            CartLine cartLine = _lines.FirstOrDefault(a => a.Product.ProductID == product.ProductID);
+            if (cartLine == null)
+            {
+                _lines.Add(new CartLine {Product = product, Quantity = quantity});
+            }
+            else
+            {
+                cartLine.Quantity += quantity;
+            }
+        }
+
+        public void RemoveFromCart(Product product)
+        {
+            _lines.RemoveAll(a => a.Product.ProductID == product.ProductID);
+        }
+
+        public decimal Total
+        {
+            get { return _lines.Sum(a => a.Product.UnitPrice * a.Quantity); }
+        }
+
+        public void Clear()
+        {
+            _lines.Clear();
+        }
+
+        public List<CartLine> Lines
+        {
+            get { return _lines; }
+        }
+    }
+
+    public class CartLine
+    {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ AspNetMvcNorthwind.MvcWebUI/Controllers/CartController.cs
@@ -0,0 +1,49 @@
+﻿using System.Web.Mvc;
+using AspNetMvcNorthwind.Entities;
+using AspNetMvcNorthwind.Interfaces;
+
+namespace AspNetMvcNorthwind.MvcWebUI.Controllers
+{
+    public class CartController : Controller
+    {
+        private IProductService _productService;
+
+        public CartController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public ViewResult Index()
+        {
+            var cart = (Cart) Session["cart"];
+            return View(cart);
+        }
+        public RedirectToRouteResult AddToCart(int productId)
+        {
+            Product product = _productService.Get(productId);
+            var cart = (Cart)Session["cart"];
+            if (cart == null)
+            {
+                cart = new Cart();
+                Session["Cart"] = cart;
+            }
+            cart.AddToCart(product,1);
+            return RedirectToAction("Index",cart);
+
+        }
+        public RedirectToRouteResult RemoveFromCart(int productId)
+        {
+            Product product = _productService.Get(productId);
+            var cart = (Cart) Session["cart"];
+            if (cart.Lines.Count == 0)
+            {
+                ModelState.AddModelError("", "Sepette zaten ürün yok");
+            }
+            else
+            {
+                cart.RemoveFromCart(product);
+            }
+            return RedirectToAction("Index", cart);
+        }
+    }
+}
