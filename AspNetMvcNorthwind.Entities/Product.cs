1fa2e4f0419e808f83d4fb3770d7f884902368ca
gokerhabib <gokerhabib@gmail.com>
2020-07-23 15:54:48 +0300

    AdminController'ın oluşturulması Admin panel Product Create,Edit,Delete işlemlerinin yapılması

--- AspNetMvcNorthwind.Entities/Product.cs
+++ AspNetMvcNorthwind.Entities/Product.cs
@@ -1,17 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Mvc;
 
 namespace AspNetMvcNorthwind.Entities
 {
     public class Product
     {
+        [HiddenInput(DisplayValue = false)]
         public int ProductID { get; set; }
+        [Required]
         public string ProductName { get; set; }
+        [Required]
         public Int16 UnitsInStock { get; set; }
         public decimal UnitPrice { get; set; }
+        [Required]
         public int CategoryID { get; set; }
     }
 }
---
+++ AspNetMvcNorthwind.MvcWebUI/Controllers/AdminController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AspNetMvcNorthwind.Entities;
+using AspNetMvcNorthwind.Interfaces;
+
+namespace AspNetMvcNorthwind.MvcWebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IProductService _productService;
+
+        public AdminController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public ActionResult Index()
+        {
+            return View(_productService.GetAll());
+        }
+
+        public ActionResult CreateNew()
+        {
+            return View(new Product());
+        }
+        [HttpPost]
+        public ActionResult CreateNew(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _productService.Add(product);
+                return RedirectToAction("Index");
+            }
+
+            return View(product);
+        }
+        public ActionResult Edit(int productId)
+        {
+            Product product = _productService.Get(productId);
+            return View(product);
+        }
+        [HttpPost]
+        public ActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _productService.Update(product);
+                return RedirectToAction("Index");
+            }
+
+            return View(product);
+        }
+        public ActionResult Delete(int productId)
+        {
+            _productService.Delete(productId);
+            return RedirectToAction("Index");
+        }
+    }
+}
